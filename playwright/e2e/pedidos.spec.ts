b1b931ca28d91ce3b94e8c744993832c1d5aec49
lacerdaRodrigo <lacerdaaa.rodrigo@gmail.com>
2026-03-01 01:54:58 -0300

    Criando mais testes

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -4,6 +4,9 @@ import { test, expect } from '@playwright/test';
 //PAV - Preparar, Agir, Verificar
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+
+    const pedido = 'VLO-28JC4G'
+
     // Arrange (preparar)
     await page.goto('http://localhost:5173/');
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
@@ -11,10 +14,39 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
     // Act (Agir)
-    await page.getByTestId('search-order-id').fill('VLO-28JC4G');
+    await page.getByTestId('search-order-id').fill(pedido);
     await page.getByTestId('search-order-button').click();
 
     // Assert (verificar)
-    await expect(page.getByText('VLO-28JC4G')).toBeVisible({timeout: 10_000});
-    await expect(page.getByText('APROVADO')).toBeVisible();
+
+    const consultaPedido = page.getByRole('paragraph')
+        .filter({ hasText: /^Pedido$/ })
+        .locator('..') // sobe para o elemento pai
+
+    await expect(consultaPedido).toContainText(pedido, { timeout: 10_000 })
+
+    await expect(page.getByText('APROVADO')).toBeVisible()
 });
+
+test('deve exibir mensagem quando o pedido não e encontrado', async ({ page }) => {
+
+    const pedido = 'ABC-28JC4G'
+
+    // Arrange (preparar)
+    await page.goto('http://localhost:5173/');
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+    // Act (Agir)
+    await page.getByTestId('search-order-id').fill(pedido);
+    await page.getByTestId('search-order-button').click();
+
+    // Assert(Verificar)
+    await expect(page.locator('#root')).toMatchAriaSnapshot(`
+        
+        - img
+        - heading 'Pedido não encontrado' [level=3]
+        - paragraph: 'Verifique o número do pedido e tente novamente'
+        `)
+})
